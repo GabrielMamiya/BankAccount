1ab40ece0ebfb7111bf0d4cc5bb44d362a6abc3c
Gabriel Tamura <gabriel-tamura@hotmail.com>
2018-03-14 23:03:51 -0300

    Initial check-in of module bankAccount

---
+++ Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+namespace bankAccount
+{
+    public class Account
+    {
+
+        public int account { get; private set; }
+        public string name { get; set; }
+        public double saldo { get; private set; }
+
+        public Account(int account, string name, double saldo)
+        {
+            this.account = account;
+            this.name = name;
+            this.saldo = saldo;
+        }
+
+        public Account(int account, string name){
+            this.account = account;
+            this.name = name;
+            this.saldo = 0;
+        }
+
+        public void showNewClient(){
+            Console.WriteLine();
+            Console.WriteLine("Account has been created.");
+            Console.WriteLine("Account: " + account);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Balance: " + saldo.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        public void addMoney(double deposit){
+
+            saldo += deposit;
+
+            Console.WriteLine();
+            Console.WriteLine("Account atualizada.");
+            Console.WriteLine("Account: " + account);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Balance: " + saldo.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        public void sacarMoney(double value){
+
+            saldo = (saldo - value) - 5;
+
+            Console.WriteLine();
+            Console.WriteLine("Account atualizada.");
+            Console.WriteLine("Account: " + account);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Balance: " + saldo.ToString("F2", CultureInfo.InvariantCulture));
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace bankAccount
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Account person1;
+
+            Console.Write("Digite o numero da conta: ");
+            int account = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do Titular da conta: ");
+            string name = Console.ReadLine();
+            Console.Write("Havera deposito inicial ? (S/N): ");
+            char response = char.Parse(Console.ReadLine());
+
+            if(response == 'S'){
+                Console.Write("Digite o deposito inicial da conta: ");
+                double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                person1 = new Account(account, name, saldo);
+                person1.showNewClient();
+                Console.Write("Digite um valor para deposito: ");
+                double deposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                person1.addMoney(deposit);
+                Console.Write("Digite um valor para saque: ");
+                double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                person1.sacarMoney(value);
+            } else if (response == 'N'){
+                person1 = new Account(account, name);
+                person1.showNewClient();
+                Console.Write("Digite um valor para deposito: ");
+                double deposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                person1.addMoney(deposit);
+                Console.Write("Digite um valor para saque: ");
+                double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                person1.sacarMoney(value);
+            }
+
+
+
+
+
+        }
+    }
+}
